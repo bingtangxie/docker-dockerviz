b63c5b1ce828af0df744a6f8b7af1f1f0ff5990c
Peter Salvatore <peter@psftw.com>
2014-09-23 09:34:06 -0700

    ignore error output in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker kill dockerviz
-docker rm dockerviz
+docker kill dockerviz &> /dev/null
+docker rm dockerviz &> /dev/null
 docker run -d --name dockerviz -p 5000:5000 \
            -v /var/run/docker.sock:/var/run/docker.sock \
            -v $(pwd):/usr/src/app \
